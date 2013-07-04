d243be07016742ab129cb0e48ae64c0f5e0733fa
Neb <bn.gladstone@gmail.com>
2013-07-04 22:32:37 +0100

    Finally Working (Very Basic) Game

---
+++ README.txt
@@ -0,0 +1,13 @@
+--Sam likes Woffles!--
+This is a very simple game, as a bit of python pratice.
+First set the height and width of the Grid you want to play in.
+Then you set the X and Y starting position of "Sam".
+Some were else on a grid a Woffle will spawn ("#").
+Then you need to use the WASD keys to move Sam to the woffle.
+
+Notes:
+	1.Choose W, A, S or D then press enter, wait for grid to redraw before doing anything else.
+	2.Very untidy code needs a good clean up.
+	3.Planning on adding terrain which you have to navigate. Maybe difficulty system?
+	Easy = 25% of Grid is covered. Medium = 50% of Grid is covered. Hard = 25% of Grid is covered.
+	4.When I have cleaned up code I will put comments in it so you can see whats happening (some vauge variable names).
--- move.py
+++ move.py
@@ -1,21 +1,41 @@
 # -*- coding: cp1252 -*-
+from random import randint
+import sys
+
 print "Welcome to the this Test"
-print "This is test 2: Organic Grid!"
+print "This is test 2: Sam like Woffles!"
 print
 
-oY = input("How tall do you want your grid?")
-oX = input("How wide do you want your grid?")
-oY = oY - 1
-oX = oX - 2
+gY = input("How tall do you want your grid?")
+gX = input("How wide do you want your grid?")
+oY = gY
+oX = gX - 2
 
 Xinput = input("Where do you want to start? (X)")
 Yinput = input("Where do you want to start (Y)?")
+print
+
+
+c = 0
+while (c == 0):
+        foodX = randint(0,oX)
+        foodY = randint(0,oY)
+        if Xinput == foodX and foodY == Yinput:
+                c = 0
+        else:
+                c = 1
+
+fX = foodX
+fY = foodY
+
+item = '#'
 
 mX = Xinput
 mY = Yinput
 
 game = 0
-ship='^'
+ship='A'
+jump = False
 
 while(game == 0):
         o = 0
@@ -27,19 +47,27 @@ while(game == 0):
 
         while (y <= oY):
                 while (x <= oX):
-                        rowXdef[y].append(o)
-                        x = x + 1
+                         rowXdef[y].append(o)
+                         x = x + 1
                 x = 0
                 y = y + 1
                 rowXdef[y] = [o]
 
-
         mark = rowXdef[mY]
         mark[mX] = ship
 
+        spawn = rowXdef[fY]
+        spawn[fX] = item
+        
         del rowXdef[(oY + 1)]
+        del rowXdef[oY]
 
-
+        if mY == fY and mX == fX:
+                print 'WIN'
+                raw_input()
+                sys.exit()
+                
+        
         for line in rowXdef:
                 x = rowXdef[line]
                 cCell = 0
@@ -47,43 +75,51 @@ while(game == 0):
                         for cell in x:
                                 print cell,
                                 cCell = cCell + 1
-                        print "\n"
-        move = raw_input()
+                        print
         clean = 0
-
         
         while (clean == 0):
+                move = raw_input()
+                
                 if move == 'w':
-                        if mY != 0:
-                                mY = mY - 1
+                        if jump == True:
+                                mY = (gY - 1)
+                                jump = False
                         else:
-                                mY = Yinput
-                        ship = '^'
+                                mY = mY - 1
+                                if mY == 0:
+                                        jump = True
+                        ship = 'A'
                         clean = 1
                 elif move == 'a':
-                        if mX != 0:
-                                mX = mX - 1
+                        if jump == True:
+                                mX = (gX - 1)
+                                jump = False
                         else:
-                                mX = Xinput
+                                mX = mX - 1
+                                if mX == 0:
+                                        jump = True
                         ship = '<'
                         clean = 1
                 elif move == 'd':
-                        if mX != Xinput:
-                                mX = mX + 1
-                        else:
+                        if jump == True:
                                 mX = 0
+                                jump = False
+                        else:
+                                mX = mX + 1
+                                if mX == (gX - 1):
+                                        jump = True
                         ship = '>'
                         clean = 1
                 elif move == 's':
-                        if mY != Yinput:
-                                mY = mY + 1
-                        else:
+                        if jump == True:
                                 mY = 0
-                        ship = 'v'
+                                jump = False
+                        else:
+                                mY = mY + 1
+                                if mY == (gY - 1):
+                                        jump = True
+                                ship = 'V'
                         clean = 1
                 else:
                         print "Ïnvalid Movement Key"
-        
-
-#Range Error?
-#Button row doesn't exist
